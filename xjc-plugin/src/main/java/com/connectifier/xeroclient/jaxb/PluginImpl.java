32c6fe15d9b92d6a70d5d8f19b348a69b5c0beff
Ben McCann <ben@connectifier.com>
2015-04-15 23:59:57 -0700

    Improve setters on classes generated from XSDs

--- xjc-plugin/src/main/java/com/connectifier/xeroclient/jaxb/PluginImpl.java
+++ xjc-plugin/src/main/java/com/connectifier/xeroclient/jaxb/PluginImpl.java
@@ -4,7 +4,6 @@ package com.connectifier.xeroclient.jaxb;
 
 import java.io.StringWriter;
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.Iterator;
 import java.util.List;
 
@@ -19,6 +18,7 @@ import com.sun.codemodel.JFieldVar;
 import com.sun.codemodel.JFormatter;
 import com.sun.codemodel.JMethod;
 import com.sun.codemodel.JOp;
+import com.sun.codemodel.JType;
 import com.sun.tools.xjc.Options;
 import com.sun.tools.xjc.Plugin;
 import com.sun.tools.xjc.outline.ClassOutline;
@@ -41,6 +41,7 @@ public class PluginImpl extends Plugin {
     JCodeModel model = outline.getModel().codeModel;
     for (ClassOutline co : outline.getClasses()) {
       updateArrayOfGetters(co, model);
+      updateArrayOfSetters(co, model);
     }
 
     return true;
@@ -54,8 +55,7 @@ public class PluginImpl extends Plugin {
     JDefinedClass implClass = co.implClass;
 
     List<JMethod> removedMethods = new ArrayList<>();
-    Collection<JMethod> methods = implClass.methods();
-    Iterator<JMethod> iter = methods.iterator();
+    Iterator<JMethod> iter = implClass.methods().iterator();
     while (iter.hasNext()) {
       JMethod method = iter.next();
       if (method.type().name().startsWith("ArrayOf")) {
@@ -84,5 +84,43 @@ public class PluginImpl extends Plugin {
               field.invoke("get" + fieldName.substring(0, 1).toUpperCase() + fieldName.substring(1))));
     }    
   }
-  
+
+  /**
+   * Update setters to use Java List. For example:
+   * setLineItems(ArrayOfLineItem value) -> setLineItems(List<LineItem> value)
+   */
+  private void updateArrayOfSetters(ClassOutline co, JCodeModel model) {
+    JDefinedClass implClass = co.implClass;
+
+    List<JMethod> removedMethods = new ArrayList<>();
+    Iterator<JMethod> iter = implClass.methods().iterator();
+    while (iter.hasNext()) {
+      JMethod method = iter.next();
+      if (method.params().size() == 1 && method.params().get(0).type().name().startsWith("ArrayOf")) {
+        removedMethods.add(method);
+        iter.remove();
+      }
+    }
+
+    for (JMethod removed : removedMethods) {
+      // Parse the old code to get the variable name
+      StringWriter oldWriter = new StringWriter();
+      removed.body().state(new JFormatter(oldWriter));
+      String oldBody = oldWriter.toString();
+      String varName = oldBody.substring(oldBody.indexOf("this.") + "this.".length(), oldBody.indexOf(" = "));
+
+      // Build the new method
+      JType arrType = removed.params().get(0).type();
+      String type = arrType.name().substring("ArrayOf".length());
+      JFieldVar field = implClass.fields().get(varName);  
+      String fieldName = model._getClass(field.type().fullName()).fields().keySet().iterator().next();
+
+      JMethod newMethod = implClass.method(removed.mods().getValue(), Void.TYPE, removed.name());
+      newMethod.param(model.ref("java.util.List").narrow(model.ref(type)), "value");
+      newMethod.body().decl(arrType, "arr", JExpr._new(arrType));
+      newMethod.body().directStatement("arr.get" + fieldName.substring(0, 1).toUpperCase() + fieldName.substring(1) + "().addAll(value);");
+      newMethod.body().directStatement("this." + varName + " = arr;");
+    }
+  }
+
 }
